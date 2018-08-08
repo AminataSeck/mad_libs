1a6bc7d4fb3320550d4c4633f1faf9e4f32e6cbf
Brenda Jacobo <brendajacobo17@gmail.com>
2018-08-08 18:23:27 +0000

    add

--- mad_libs_app/main.py
+++ mad_libs_app/main.py
@@ -2,7 +2,7 @@ import webapp2
 from random import shuffle
 import jinja2
 import os
-from models import MadLib
+#from models import MadLib
 
 #libraries for APIs
 from google.appengine.api import urlfetch
