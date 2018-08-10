0b89c8ba18b9c9b1f8ed7c90981b2e98430c3aa5
Brenda Jacobo <brendajacobo17@gmail.com>
2018-08-10 02:21:11 +0000

    noun working

--- mad_libs_app/main.py
+++ mad_libs_app/main.py
@@ -110,14 +110,40 @@ class UniPage(webapp2.RequestHandler):
 class TechPage(webapp2.RequestHandler):
     def post(self):
         about_template = the_jinja_env.get_template('templates/Result_page.html')
-        noun = self.request.get("noun")
+        noun1 = self.request.get("noun1")
+        noun2 = self.request.get("noun2")
+        noun3 = self.request.get("noun3")
+        noun4 = self.request.get("noun4")
+        noun5 = self.request.get("noun5")
+        noun6 = self.request.get("noun6")
+        noun7 = self.request.get("noun7")
+        noun8 = self.request.get("noun8")
+        noun9 = self.request.get("noun9")
+        noun10 = self.request.get("noun10")
+        noun11= self.request.get("noun11")
+        noun12= self.request.get("noun12")
+        noun13= self.request.get("noun13")
+        noun14= self.request.get("noun14")
         verb = self.request.get("verb")
         adjective = self.request.get("adj")
         number = self.request.get("num")
         adverb = self.request.get("adv")
         the_variable_dict = {
             
-            "noun_key": noun, 
+            "noun1_key": noun1, 
+            "noun2_key": noun2, 
+            "noun3_key": noun3, 
+            "noun4_key": noun4, 
+            "noun5_key": noun5, 
+            "noun6_key": noun6, 
+            "noun7_key": noun7, 
+            "noun8_key": noun8, 
+            "noun9_key": noun9, 
+            "noun10_key": noun10, 
+            "noun11_key": noun11, 
+            "noun12_key": noun12, 
+            "noun13_key": noun13, 
+            "noun14_key": noun4, 
             "verb_key": verb,
             "adj_key": adjective,
             "num_key": number,
@@ -131,64 +157,64 @@ class TechPage(webapp2.RequestHandler):
         
         mad_lib_inputs =[
             {
-                "part_of_speech": "adjective",
+                "part_of_speech": "adjective1",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun1",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun2",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun3",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun4",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun5",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun6",
             },
             {
-                 "part_of_speech": "adjective",
+                 "part_of_speech": "adjective2",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun7",
             },
             {
-                 "part_of_speech": "adjective",
+                 "part_of_speech": "adjective3",
             },
             {
-                 "part_of_speech": "verb",
+                 "part_of_speech": "verb1",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun8",
             },
             {
-                 "part_of_speech": "verb",
+                 "part_of_speech": "verb2",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun9",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun10",
             },
             {
-                 "part_of_speech": "number",
+                 "part_of_speech": "number1",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun11",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun12",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun13",
             },
             {
-                 "part_of_speech": "noun",
+                 "part_of_speech": "noun14",
             }
         ]    
 
@@ -239,7 +265,7 @@ class SportsPage(webapp2.RequestHandler):
             },
             {
                  "part_of_speech": "adjective",
-            
+            },
             {
                  "part_of_speech": "adjective",
             },
@@ -299,6 +325,7 @@ class SportsPage(webapp2.RequestHandler):
         the_variable_dict = {
             
             "chosen_madlib": mad_lib_inputs
+            
             
         }
         
